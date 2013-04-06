b6b6548ecd4f8786ab2534a1837db94c1eff5606
grosbouddha <grosbouddha@gmail.com>
2013-04-06 18:01:33 +0200

    Fixes
    Add dynamic debug parameter
    Add window focus/blur events
    fix publish_shot naming inconsistency
    add non-selectability of meta text

--- src/main.js
+++ src/main.js
@@ -1,6 +1,3 @@
-
-window.debug = true ;
-
 if(typeof DH == "undefined") {
 	window.DH = {} ;
 }
@@ -14,24 +11,39 @@ YUI().use('node', function(Y) {
 		    		AppConf.gameCanvasInfo.height,
 		    		{'cursor' : 'crosshair'});
 
-			DH.gameCanvas.publish_shot = function(evt) {
+			DH.gameCanvas.publishShot = function(evt) {
 		    	DH.publisher.fire(DH.Events.TRIGGER_PULL);
 		    };
 
 			DH.gameCanvas.activateGun = function() {
 				if(!DH.gameCanvas.isGunActive) {
-					DH.gameCanvas.addEventListener("click", DH.gameCanvas.publish_shot);
+					DH.gameCanvas.addEventListener("click", DH.gameCanvas.publishShot);
 					DH.gameCanvas.isGunActive = true ;
 				}
 			};
 
 			DH.gameCanvas.deactivateGun = function() {
-				DH.gameCanvas.removeEventListener("click", DH.gameCanvas.publish_shot);
+				DH.gameCanvas.removeEventListener("click", DH.gameCanvas.publishShot);
 				DH.gameCanvas.isGunActive = false ;
 			};
 
-		    if(AppConf.debug) {
+			DH.gameCanvas.onWindowBlur = function() {
+				DH.publisher.fire(DH.Events.WINDOW_BLUR);
+			};
 
+			DH.gameCanvas.onWindowFocus = function() {
+				DH.publisher.fire(DH.Events.WINDOW_FOCUS);
+			};
+
+			if (document.onfocusin) {
+				document.onfocusin = DH.gameCanvas.onWindowFocus;
+				document.onfocusout = DH.gameCanvas.onWindowBlur;
+			} else {
+				window.onfocus = DH.gameCanvas.onWindowFocus;
+				window.onblur = DH.gameCanvas.onWindowBlur;
+			}
+
+		    if(AppConf.debug) {
 		    	DH.gameCanvas.addEventListener("click", function(evt) {
 		    		window.document.title = "x: " + evt.layerX + " - y: "+evt.layerY;
 			    });
